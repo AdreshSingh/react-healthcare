ff89b1113ce96a58bb7c3b5df6ce05377cd353f3
AdreshSingh <adreshsingh271527@gmail.com>
2024-11-20 01:42:51 +0530

    a simple dashboard added

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,30 @@
+import { useSave } from "../hooks/useSave"
+
+
+
+export const Dashboard = () => {
+    const { saveSate } = useSave()
+    const userData = saveSate
+    console.log(userData)
+
+    return <>
+        <table className="w-3/4 p-6 m-auto mt-5 text-center bg-blue-100 shadow-md">
+            <thead>
+                <tr className="border-b-2 border-gray-400">
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Age
+                    </th>
+                </tr>
+            </thead>
+            {
+                saveSate.map((user, index) => (<tr key={index} className="border-b-2  even:border-gray-300">
+                    <td>{user.name}</td>
+                    <td>{user.age}</td>
+                </tr>))
+            }
+        </table>
+    </>
+}
