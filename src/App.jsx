e218e7b2a99ed5fb261496a8240b94cd9448a945
AdreshSingh <adreshsingh271527@gmail.com>
2024-11-20 01:42:11 +0530

    altered alyout react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 // import { useState } from 'react'
-import Form from './components/Form'
+import { Link, Outlet } from 'react-router-dom'
 
 
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      <main className='min-h-screen bg-gray-200'>
-        <div className='bg-pink-300 p-2 flex items-center'>
-          <h1 className='text-2xl text-white font-bold'>Welcome to HealthCare</h1>
+      <main className='min-h-screen bg-gray-100'>
+        <div className='flex items-center justify-between p-3 bg-blue-300'>
+          <h1 className='p-2 text-2xl font-bold rounded-sm text-slate-100 ring ring-slate-200'>
+            <Link to={"/"}>Welcome to HealthCare</Link>
+          </h1>
+          <Link to={`/dashboard`} className="p-1 bg-blue-100 rounded-md focus-visible:bg-blue-300 hover:bg-blue-200 hover:ring-1 hover:ring-blue-600">Dashboard</Link>
         </div>
-        <div className='h-screen flex flex-col justify-center items-center'>
-          <Form />
-        </div>
+        <Outlet />
       </main>
     </>)
 }
