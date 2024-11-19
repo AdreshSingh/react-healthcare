0c85c48c954347a55a0f033c0d1cc727686916a2
AdreshSingh <adreshsingh271527@gmail.com>
2024-11-20 01:42:30 +0530

    altered for react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Form from './components/Form.jsx'
+import { Dashboard } from './components/Dashboard.jsx'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />,
+  children: [
+    { path: "/", element: <Form /> },
+    { path: "/dashboard", element: <Dashboard /> },
+  ]
+}], { future: { v7_fetcherPersist: true, } })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
